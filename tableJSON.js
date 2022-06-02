1d877d4d934afb20f69c925053df9ace93c8d774
Hangs Breaker <hangsbreaker@gmail.com>
2022-06-02 10:26:15 +0700

    Update tableJSON.js

--- tableJSON.js
+++ tableJSON.js
@@ -27,3 +27,27 @@ function tableJSON(tableid) {
   }
   return res;
 }
+
+function tableStrJSON(table){
+    let keys = [],res = [];
+    var tr = table.match(/<tr[^>]*>([\s\S]*?)<\/tr>/g);
+    for(t in tr){
+        tr[t] = tr[t].replace(/<tr[^>]*>/g,"").replace(/<\/tr>/g,"").trim();
+        var td = tr[t].match(/<th[^>]*>([\s\S]*?)<\/th>/g);
+        if(td==null){
+            td = tr[t].match(/<td[^>]*>([\s\S]*?)<\/td>/g);
+        }
+        if(t==0){
+            for(d in td){
+                keys.push(striptags(td[d]).replace(/[^A-Za-z0-9!?]/g, ""));
+            }
+        }else{
+            var obj = {};
+            for(d in td){
+                obj[keys[d]] = striptags(td[d]);
+            }
+            res.push(obj);
+        }
+    }
+    return res;
+}
